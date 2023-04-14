5b89fc63626bd8594794c1425bae2737de3db163
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-04-14 14:50:21 +0700

    selesai membuat fungsi input

--- LinearSearch_087/LinearSearch_087.cpp
+++ LinearSearch_087/LinearSearch_087.cpp
@@ -6,5 +6,22 @@ int n; // Number of element in the array
 int i; // Index of array element
 
 void input() {
+	while (true)
+	{
+		cout << "Enter the number og elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maksimum 20 elements.\n\n";
+	}
 
+	// Accept array elements
+	cout << "\n====================\n";
+	cout << "Enter array elements\n";
+	cout << "\n====================\n";
+	for (i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
